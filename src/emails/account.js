4287d14fab87cfdedd877519cbc881d33ee1dd17
Nitin Dhiman <nitin.dhiman1997.nd@gmail.com>
2020-04-25 13:42:01 +0530

    Changed Mail Limit for App

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,26 +1,50 @@
 const sendGridKey = process.env.MAIL_KEY;
 const sgMail = require('@sendgrid/mail');
 
+let cnt = 0;
+
+// 86400000
+setInterval(() => {
+    cnt = 0
+}, 86400000);
+
 sgMail.setApiKey(sendGridKey);
 
 const sendWelcomeMail = (email, name) => {
-    sgMail.send({
+    const message = {
         to: email,
         from: 'nitin.dhiman1997.nd@gmail.com',
         subject: 'Welcome to TaskApp!',
         text: `Hi ${name}.\nHope you enjoy the Task App!`
-    });
-    console.log(`Sent welcome mail to ${email}!`)
+    };
+
+    if (cnt <= parseInt(process.env.MAIL_LIMIT, 10)) {
+        sendEmail(message);
+        console.log(`Sent welcome mail to ${email}!`);
+        cnt += 1;
+    } else {
+        console.log('Limit Crossed for mail!')
+    }
 };
 
 const sendDeletedAccountMail = (email, name) => {
-    sgMail.send({
+    const message = {
         to: email,
         from: 'nitin.dhiman1997.nd@gmail.com',
-        subject: 'Your Acccount has been removed!',
+        subject: 'Your Account has been removed!',
         text: `Hi ${name}.\nYour account has been deleted from Task App. Please share your feedback.`
-    });
-    console.log(`Sent Deleted account mail to ${email}!`)
+    };
+    if (cnt <= parseInt(process.env.MAIL_LIMIT, 10)) {
+        sendEmail(message);
+        console.log(`Sent Deleted account mail to ${email}!`);
+        cnt += 1;
+    } else {
+        console.log('Limit Crossed for mail!')
+    }
+};
+
+const sendEmail = (message) => {
+    sgMail.send(message)
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 const express = require('express');
 require('./db/mongoose');
+require('./emails/account');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// setup multer
-// const multer = require('multer');
-// const upload = multer({
-//     dest: 'images'
-// });
-// app.post('/up', upload.single('ipload'), (req, res) => {
-//     res.send();
-// });
 
 app.use(express.json());
 app.use(userRouter);
@@ -22,3 +14,5 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server running at ' + port);
 });
+
+
